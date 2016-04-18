67c069313b117fd355b2329feaaa35feadc7cd60
nopphon <nopphonyel@gmail.com>
2016-04-18 23:14:42 +0700

    Add a card detail class

--- app/src/main/java/com/urandom/utech/u_random/CardDetail.java
+++ app/src/main/java/com/urandom/utech/u_random/CardDetail.java
@@ -5,46 +5,5 @@ package com.urandom.utech.u_random;
  * Created by nopphon on 4/12/16.
  */
 public class CardDetail {
-    private String songTitle , artist , hashTag , source;
-    private String URL;
 
-    public String getURL() {
-        return URL;
-    }
-
-    public void setURL(String URL) {
-        this.URL = URL;
-    }
-
-    public String getSongTitle() {
-        return songTitle;
-    }
-
-    public void setSongTitle(String songTitle) {
-        this.songTitle = songTitle;
-    }
-
-    public String getArtist() {
-        return artist;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
-    }
-
-    public String getHashTag() {
-        return hashTag;
-    }
-
-    public void setHashTag(String hashTag) {
-        this.hashTag = hashTag;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
 }
